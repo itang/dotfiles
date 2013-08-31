c846c2873366601c33e8c977aba4a61f174804d6
itang <live.tang@gmail.com>
2013-08-31 13:58:16 +0800

    plugin cloc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-clojars "0.9.1"]
                   [lein-try "0.3.0"]
-                  [lein-ancient "0.4.4"]]
+                  [lein-ancient "0.4.4"]
+                  [cloc "0.1.0"]]
         :signing {:gpg-key "2852B4FF"}}}
