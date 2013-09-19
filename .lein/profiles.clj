eedbbc3b6807c24fee07e6722cf2e5134f9456b4
itang <live.tang@gmail.com>
2013-09-19 08:20:01 +0800

    u lein/profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-clojars "0.9.1"]
-                  [lein-try "0.3.0"]
+                  [codox "0.6.6"]
+                  [lein-try "0.3.1"]
                   [lein-ancient "0.4.4"]
-                  [cloc "0.1.0"]]
+                  [cloc "0.1.0"]
+                  [lein-typed "0.3.0"]]
         :signing {:gpg-key "2852B4FF"}}}
