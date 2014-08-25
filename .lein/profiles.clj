cd69648968ddcd009ee0017ef21be0a4ceb71c14
itang <live.tang@gmail.com>
2014-08-25 22:22:49 +0800

    update lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
-{:user {:plugins [[lein-clojars "0.9.1"]
-                  [codox "0.6.6"]
-                  [lein-try "0.3.1"]
-                  [lein-ancient "0.4.4"]
+{:user {:plugins [#_[lein-clojars "0.9.1"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [codox "0.8.10"]
+                  [lein-try "0.4.3"]
+                  [lein-ancient "0.5.5"]
                   [cloc "0.1.0"]
-                  [lein-typed "0.3.0"]]
+                  [lein-typed "0.3.5"]]
         :signing {:gpg-key "2852B4FF"}}}
